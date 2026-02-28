5e651f78a5a1c4b0e89ab941291e5d13e25cc860
zuñiga <167140329+zunigx@users.noreply.github.com>
2026-02-27 20:30:37 -0600

    Change health route to healthOK

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/health', (req, res) => {
+router.get('/healthOK', (req, res) => {
   res.status(200).json({ status: 'OK', uptime: process.uptime() })
 })
 
