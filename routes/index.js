a025339d2f4e2c50961409637091921ae7a5fab2
zuñiga <167140329+zunigx@users.noreply.github.com>
2026-02-27 20:32:09 -0600

    Rename healthOK endpoint to health

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/healthOK', (req, res) => {
+router.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK', uptime: process.uptime() })
 })
 
